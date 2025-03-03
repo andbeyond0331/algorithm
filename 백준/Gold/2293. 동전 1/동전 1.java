83e2ca164db65ec7bd2fc3e95079b78d1bc69cd0
PARK MI JUNG <88870452+andbeyond0331@users.noreply.github.com>
2025-03-03 18:30:36 +0900

    [Gold IV] Title: 동전 1, Time: 108 ms, Memory: 14100 KB -BaekjoonHub

--- 백준/Gold/2293. 동전 1/동전 1.java
+++ 백준/Gold/2293. 동전 1/동전 1.java
@@ -1,33 +1,26 @@
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int[] memo;
-	static int[] coins;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		int n = Integer.parseInt(st.nextToken());
 		int k = Integer.parseInt(st.nextToken());
-
-		memo = new int[k + 1];
-		coins = new int[n];
-		for (int i = 0; i < n; i++) {
+		int[] coins = new int[n];
+		for(int i =0; i < n; i++) {
 			coins[i] = Integer.parseInt(br.readLine());
 		}
-		memo[0] = 1;
-		for(int num: coins) {
-			for(int i = num; i <=k; i++) {
-				memo[i] += memo[i-num];
+		int[] dp = new int[k+1];
+		dp[0] = 1;
+		for(int coin: coins) {
+			for(int i = coin; i <=k; i++) {
+				dp[i] += dp[i-coin];
 			}
 		}
-//		for(int num : memo) {
-//			System.out.print(num + " ");
-//		}
-		System.out.println(memo[k]);
+		System.out.println(dp[k]);
 	}
+
 }
