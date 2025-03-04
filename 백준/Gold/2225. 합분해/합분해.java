1eb7b376978332cbe36ba03e8529ef9523e98dac
PARK MI JUNG <88870452+andbeyond0331@users.noreply.github.com>
2025-03-04 17:35:21 +0900

    [Gold V] Title: 합분해, Time: 112 ms, Memory: 14296 KB -BaekjoonHub

---
+++ 백준/Gold/2225. 합분해/합분해.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int n, k;
+	static int[][] dp;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken());
+		k = Integer.parseInt(st.nextToken());
+		dp = new int[n+1][k+1];
+		for(int i = 0; i <=n; i++) {
+			dp[i][0] = 0;
+			dp[i][1] = 1;
+		}
+		for(int i = 0; i <=k; i++) {
+			dp[1][i] = i;
+		}
+		for(int i = 2; i <=n; i++) {
+			for(int j = 2; j <= k; j++) {
+				dp[i][j] = (dp[i-1][j] + dp[i][j-1])%1000000000;
+			}
+		}
+		System.out.println(dp[n][k]);
+	}
+}
