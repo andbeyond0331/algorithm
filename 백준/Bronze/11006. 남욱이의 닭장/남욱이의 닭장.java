b93d5b04c8801d8188c6eacaad99e08225e1a6f9
PARK MI JUNG <88870452+andbeyond0331@users.noreply.github.com>
2025-03-07 17:58:54 +0900

    [Bronze III] Title: 남욱이의 닭장, Time: 120 ms, Memory: 14412 KB -BaekjoonHub

---
+++ 백준/Bronze/11006. 남욱이의 닭장/남욱이의 닭장.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st;
+    int T = Integer.parseInt(br.readLine());
+    for(int i = 0;i < T; i++) {
+      st = new StringTokenizer(br.readLine());
+      int n = Integer.parseInt(st.nextToken());
+      int m = Integer.parseInt(st.nextToken());
+      int u = (2*m) - n;
+      int t = m-u;
+      System.out.println(u + " "+ t);
+    }
+  }
+
+}
