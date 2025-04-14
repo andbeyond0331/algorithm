83b28175aac8fa68da0e73147b58d15261cb3db5
PARK MI JUNG <88870452+andbeyond0331@users.noreply.github.com>
2025-04-14 11:08:31 +0900

    [Bronze V] Title: 성택이의 은밀한 비밀번호, Time: 132 ms, Memory: 14752 KB -BaekjoonHub

---
+++ 백준/Bronze/25372. 성택이의 은밀한 비밀번호/성택이의 은밀한 비밀번호.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int N = Integer.parseInt(br.readLine());
+    for(int i = 0; i < N; i++) {
+      String line = br.readLine();
+      if(line.length()>=6 && line.length()<=9) {
+        System.out.println("yes");
+      }else {
+        System.out.println("no");
+      }
+    }
+  }
+}
