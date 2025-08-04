79bad24c0c9b7878f21c81983bad5a68b47b604e
PARK MI JUNG <88870452+andbeyond0331@users.noreply.github.com>
2025-08-04 13:37:11 +0900

    [Silver III] Title: 소수 구하기, Time: 552 ms, Memory: 22732 KB -BaekjoonHub

---
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -0,0 +1,62 @@
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+import static java.lang.Math.sqrt;
+
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader bufferedReader = new BufferedReader( new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter( new OutputStreamWriter(System.out));
+
+        StringBuilder stringBuilder = new StringBuilder();
+        StringTokenizer st;
+        String str = bufferedReader.readLine();
+
+        st = new StringTokenizer(str, " ");
+
+        int M = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+
+        for(int i = M; i<N+1; i++){
+
+                int count =0;
+
+                if(i==1){
+                  count=1;
+                }else if(i%2!=0){
+                    for (int j = 2; j <= sqrt(i); j++) {
+                        if(i%j==0){
+                            count++;
+                        }
+                        if(count>0){
+                            break;
+                        }
+                    }
+                }else if(i==2){
+
+                    count=0;
+                }else{
+                    count++;
+                }
+
+                if(count==0){
+                    stringBuilder.append(i).append("\n");
+                }
+
+
+        }
+
+        bufferedWriter.write(String.valueOf(stringBuilder));
+
+        bufferedWriter.flush();
+
+
+        bufferedReader.close();
+        bufferedWriter.close();
+
+    }
+}
