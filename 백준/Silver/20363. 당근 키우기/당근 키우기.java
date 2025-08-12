e7afb9f51aece8eecbba3c78c0061aa2b5f5f835
PARK MI JUNG <88870452+andbeyond0331@users.noreply.github.com>
2025-08-12 10:10:44 +0900

    [Silver IV] Title: 당근 키우기, Time: 104 ms, Memory: 14216 KB -BaekjoonHub

---
+++ 백준/Silver/20363. 당근 키우기/당근 키우기.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+ 
+public class Main {
+    // 20363번 - 당근 키우기  
+    public static void main(String[] args) throws IOException{
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        String[] xy=br.readLine().split(" ");
+        int x=Integer.parseInt(xy[0]);  //온기
+        int y=Integer.parseInt(xy[1]);  //수분
+        int sum=x+y+Math.min(x, y)/10;
+        
+        System.out.println(sum);
+    }
+}
