abbcdf9f973d7ab76512da21d6d75874af06fdff
PARK MI JUNG <88870452+andbeyond0331@users.noreply.github.com>
2025-08-15 10:43:30 +0900

    [Gold V] Title: 김밥천국의 계단, Time: 176 ms, Memory: 18592 KB -BaekjoonHub

--- 백준/Gold/28069. 김밥천국의 계단/김밥천국의 계단.java
+++ 백준/Gold/28069. 김밥천국의 계단/김밥천국의 계단.java
@@ -1,25 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
-    static int n, k;
-    static final int INF = 1000001;
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        n = sc.nextInt();
-        k = sc.nextInt();
-        int[] dp = new int[n + 1];
-        Arrays.fill(dp, INF);
-        dp[0] = 0;
 
-        for (int i = 0; i < n; i++) {
-            if (i + 1 <= n) {
-                dp[i + 1] = Math.min(dp[i + 1], dp[i] + 1);
-            }
-            if (i + i / 2 <= n) {
-                dp[i + i / 2] = Math.min(dp[i + i / 2], dp[i] + 1);
-            }
-        }
-        System.out.println(dp[n] <= k ? "minigimbob" : "water");
+  static boolean answer;
+  static int N, k;
+  static Integer[] dp;
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    N = Integer.parseInt(st.nextToken()); //계단 개수
+    k = Integer.parseInt(st.nextToken()); // 계단 오르는 횟수
+    dp = new Integer[N + 1];
+    Arrays.fill(dp, 1000001);
+    dp[0] = 0;
+    for(int i = 0; i < N; i++) {
+      if(i+1 <=N) {
+        dp[i+1] = Math.min(dp[i+1], dp[i]+1);
+      }
+      if(i+i/2 <=N) {
+        dp[i+i/2] = Math.min(dp[i+i/2], dp[i] + 1);
+      }
     }
+      System.out.println(dp[N]<=k ? "minigimbob":"water");
+  }
 }
