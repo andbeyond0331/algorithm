387cc0f110be8b3f4aee951216f6ce74ba38d680
PARK MI JUNG <88870452+andbeyond0331@users.noreply.github.com>
2025-09-13 14:34:40 +0900

    [level 2] Title: 괄호 회전하기, Time: 40.44 ms, Memory: 97 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -1,40 +1,44 @@
+import java.util.*;
+
 class Solution {
     public int solution(String s) {
         int answer = 0;
-        for(int i = 0; i < s.length(); i++) {
-            String temp;
-            temp = s.substring(i) + s.substring(0,i);
-            // System.out.println(temp);
-            if(check(temp)) {
-                answer ++;
+        Stack<Character> stack;
+        for(int idx = 0; idx < s.length(); idx++) {
+            stack = new Stack();
+            for(int i = 0;i < s.length(); i++) {
+                char c = s.charAt(i);
+                if(c=='(' || c=='[' || c=='{') {
+                    stack.add(c);
+                    
+                } else if(c==')') {
+                    if(stack.isEmpty()) {
+                        stack.add(c);
+                        break;
+                    } else if(stack.peek() == '(') {
+                        stack.pop();
+                    }
+                } else if(c==']') {
+                    if(stack.isEmpty()) {
+                        stack.add(c);
+                        break;
+                    }else if(stack.peek() == '[') {
+                        stack.pop();
+                    }
+                } else if(c=='}') {
+                    if(stack.isEmpty()) {
+                        stack.add(c);
+                        break;
+                    }else if(stack.peek() == '{') {
+                        stack.pop();
+                    }
+                }
             }
+            if(stack.isEmpty()) {
+                answer++;
+            }
+            s = s.substring(1) + s.substring(0, 1);
         }
         return answer;
-    }
-    static boolean check(String s) {
-        // System.out.println("///");
-        int len = s.length();
-        if(s.contains("()")) {
-            int idx = s.indexOf("()");
-            // System.out.println(s);
-            s = s.substring(0, idx) + s.substring(idx + 2);
-            // System.out.println(s);
-        }
-        if(s.contains("{}")) {
-            int idx = s.indexOf("{}");
-            s = s.substring(0, idx) + s.substring(idx + 2);
-        }
-        if(s.contains("[]")) {
-            int idx = s.indexOf("[]");
-            s = s.substring(0, idx) + s.substring(idx + 2);
-        }
-        
-        if(s.length()==0) {
-            return true;
-        }
-        if(s.length()==len) {
-            return false;
-        }
-        return check(s);
     }
 }
