5f8d8c0d448d6dbc3192f0e8196667800c98e613
PARK MI JUNG <88870452+andbeyond0331@users.noreply.github.com>
2026-02-07 10:25:59 +0900

    [Bronze I] Title: 열 순서, Time: 100 ms, Memory: 14232 KB -BaekjoonHub

--- 백준/Bronze/16495. 열 순서/열 순서.java
+++ 백준/Bronze/16495. 열 순서/열 순서.java
@@ -6,16 +6,16 @@ public class Main {
 
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    // 16진법인데.. 0이 없는?
+    // A
+    // AA -> 26+1
+    // AAA -> 26*26+1
+    // AAAA -> 26*26*26+1
     String line = br.readLine().trim();
     long answer = 0;
-    for (char c : line.toCharArray()) {
-//      int number = line.charAt(i)-'A'+1;
-//      answe r+= (line.length()-1-i)*(number)*26;
-      answer = answer * 26 + (c-'A'+1);
+    for(char c : line.toCharArray()) {
+      answer=answer*26+(c-'A'+1);
     }
-//    answer+=line.charAt(line.length()-1)-'A'+1;
     System.out.println(answer);
-    // AAA-> 26*26 + 26 + 1
-    //
   }
 }
