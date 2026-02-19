f3ef81a90a8c676562b095a435f6e3033b49dd9d
PARK MI JUNG <88870452+andbeyond0331@users.noreply.github.com>
2026-02-19 13:36:02 +0900

    [Silver V] Title: 수들의 합, Time: 112 ms, Memory: 14248 KB -BaekjoonHub

---
+++ 백준/Silver/1789. 수들의 합/수들의 합.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    long s = Long.parseLong(br.readLine());
+    long n = 1;
+    while(true) {
+      if((n*n + n)/2>s) {
+        n--;
+        break;
+      }
+      n++;
+    }
+    System.out.println(n);
+  }
+}
