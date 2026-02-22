0b1ed535624af2518f6728d15a11f5945019343c
PARK MI JUNG <88870452+andbeyond0331@users.noreply.github.com>
2026-02-22 19:38:28 +0900

    [Silver IV] Title: 접미사 배열, Time: 176 ms, Memory: 16008 KB -BaekjoonHub

---
+++ 백준/Silver/11656. 접미사 배열/접미사 배열.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    String s = br.readLine();
+    String[] words = new String[s.length()];
+    for (int i = 0; i < s.length(); i++) {
+      words[i] = s.substring(i);
+    }
+    Arrays.sort(words);
+    for (String word : words) {
+      System.out.println(word);
+    }
+  }
+}
