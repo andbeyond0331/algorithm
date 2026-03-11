e93a1a06938a0734e5e81b25fe7123122cee08c9
PARK MI JUNG <88870452+andbeyond0331@users.noreply.github.com>
2026-03-11 21:01:29 +0900

    [Bronze V] Title: 세제곱의 합, Time: 100 ms, Memory: 14084 KB -BaekjoonHub

---
+++ 백준/Bronze/28701. 세제곱의 합/세제곱의 합.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int N = Integer.parseInt(br.readLine());
+    int first = 0;
+    int second = 0;
+    for (int i = 1; i <= N; i++) {
+      first+=i;
+      second+=i*i*i;
+    }
+    System.out.println(first);
+    System.out.println(first*first);
+    System.out.println(second);
+  }
+}
