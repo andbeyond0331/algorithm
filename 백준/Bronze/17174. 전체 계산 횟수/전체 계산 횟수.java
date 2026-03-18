321de4e6c5032a9414342e577700dc4ad7e80c0e
PARK MI JUNG <88870452+andbeyond0331@users.noreply.github.com>
2026-03-18 13:45:37 +0900

    [Bronze III] Title: 전체 계산 횟수, Time: 104 ms, Memory: 14072 KB -BaekjoonHub

---
+++ 백준/Bronze/17174. 전체 계산 횟수/전체 계산 횟수.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    int n = Integer.parseInt(st.nextToken());
+    int m = Integer.parseInt(st.nextToken());
+    int count = 0;
+    while(true) {
+      count+= n;
+      n/=m;
+      if(n==0) {
+        break;
+      }
+    }
+    System.out.println(count);
+  }
+}
